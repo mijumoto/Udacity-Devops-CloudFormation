dce71300838e5d835eaa177b679c758a230e2f15
mijumoto <15346105+mijumoto@users.noreply.github.com>
2021-03-04 10:00:54 -0400

    bash script update

--- cloudform.sh
+++ cloudform.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 # Parameters:
-# $1 create or update stack
+# $1 create, delete or update stack
 # $2 Stack name
 # $3 Template body
 # $4 Parameters file
 
-# Example: ./cloudform.sh create infra.yml infra.json
-# Example: ./cloudform.sh update servers.yml servers.json
+# Example: ./cloudform.sh create stackName infra.yml infra.json
+# Example: ./cloudform.sh delete stackName
+# Example: ./cloudform.sh update stackName servers.yml servers.json
 
 REGION="--region=us-east-1"
 CAPABILITIES='--capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"'
 if [ "$1" = "create" ]; then
 	aws cloudformation create-stack --stack-name $2 --template-body file://$3  --parameters file://$4 $CAPABILITIES $REGION
+if [ "$1" = "delete" ]; then
+	aws cloudformation delete-stack --stack-name $2
 if [ "$1" = "update" ]; then
 	aws cloudformation update-stack --stack-name $2 --template-body file://$3  --parameters file://$4 $CAPABILITIES $REGION
 fi
-
